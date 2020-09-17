7e17a4e87a4fa48bbac4a842bdf9fffa136acf35
Lee Castelani <notayogilee@gmail.com>
2020-09-17 14:18:39 -0400

    add and delete contact functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid'; // uuid v4() to gen id
+import { v4 as uuidv4 } from 'uuid'; // uuidv4() to gen id
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -41,8 +41,16 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
+  const deleteContact = id => {
+    ;
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current Contact
 
@@ -56,7 +64,9 @@ const ContactState = props => {
 
   return (
     <ContactContext.Provider value={{
-      contacts: state.contacts
+      contacts: state.contacts,
+      addContact,
+      deleteContact
     }}>
       {props.children}
     </ContactContext.Provider>
