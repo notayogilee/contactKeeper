2f6fbca760468007fe4bb1a9e7a02f2dbcfaa125
Lee Castelani <notayogilee@gmail.com>
2020-09-17 14:49:04 -0400

    update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
         phone: '333-333-3333',
         type: 'professional'
       },
-    ]
+    ],
+    current: null
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
@@ -48,16 +49,21 @@ const ContactState = props => {
 
   // Delete Contact
   const deleteContact = id => {
-    ;
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
 
   // Set Current Contact
-
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   // Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   // Update Contact
-
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contacts
 
   // Clear Filter
@@ -65,8 +71,13 @@ const ContactState = props => {
   return (
     <ContactContext.Provider value={{
       contacts: state.contacts,
+      current: state.current,
       addContact,
-      deleteContact
+      deleteContact,
+      setCurrent,
+      clearCurrent,
+      updateContact
+
     }}>
       {props.children}
     </ContactContext.Provider>
